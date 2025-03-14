c1b40c147754740417823a5ea9729068706b1be4
Shayan Bali <shayanbali@gmail.com>
2025-03-14 15:56:47 +0000

    Update .py file

--- coronavirus_tweets.py
+++ coronavirus_tweets.py
@@ -1,67 +1,77 @@
-# Part 2: Cluster Analysis
+# Part 3: Text mining.
 
-# Return a pandas dataframe containing the data set that needs to be extracted from the data_file.
-# data_file will be populated with the string 'wholesale_customers.csv'.
-def read_csv_2(data_file):
+# Return a pandas dataframe containing the data set.
+# Specify a 'latin-1' encoding when reading the data.
+# data_file will be populated with a string 
+# corresponding to a path containing the wholesale_customers.csv file.
+def read_csv_3(data_file):
 	pass
 
-# Return a pandas dataframe with summary statistics of the data.
-# Namely, 'mean', 'std' (standard deviation), 'min', and 'max' for each attribute.
-# These strings index the new dataframe columns. 
-# Each row should correspond to an attribute in the original data and be indexed with the attribute name.
-def summary_statistics(df):
+# Return a list with the possible sentiments that a tweet might have.
+def get_sentiments(df):
 	pass
 
-# Given a dataframe df with numeric values, return a dataframe (new copy)
-# where each attribute value is subtracted by the mean and then divided by the
-# standard deviation for that attribute.
-def standardize(df):
+# Return a string containing the second most popular sentiment among the tweets.
+def second_most_popular_sentiment(df):
 	pass
 
-# Given a dataframe df and a number of clusters k, return a pandas series y
-# specifying an assignment of instances to clusters, using kmeans.
-# y should contain values in the set {0,1,...,k-1}.
-# To see the impact of the random initialization,
-# using only one set of initial centroids in the kmeans run.
-def kmeans(df, k):
+# Return the date (string as it appears in the data) with the greatest number of extremely positive tweets.
+def date_most_popular_tweets(df):
 	pass
 
-# Given a dataframe df and a number of clusters k, return a pandas series y
-# specifying an assignment of instances to clusters, using kmeans++.
-# y should contain values from the set {0,1,...,k-1}.
-def kmeans_plus(df, k):
+# Modify the dataframe df by converting all tweets to lower case. 
+def lower_case(df):
 	pass
 
-# Given a dataframe df and a number of clusters k, return a pandas series y
-# specifying an assignment of instances to clusters, using agglomerative hierarchical clustering.
-# y should contain values from the set {0,1,...,k-1}.
-def agglomerative(df, k):
+# Modify the dataframe df by replacing each characters which is not alphabetic or whitespace with a whitespace.
+def remove_non_alphabetic_chars(df):
 	pass
 
-# Given a data set X and an assignment to clusters y
-# return the Silhouette score of this set of clusters.
-def clustering_score(X,y):
+# Modify the dataframe df with tweets after removing characters which are not alphabetic or whitespaces.
+def remove_multiple_consecutive_whitespaces(df):
 	pass
 
-# Perform the cluster evaluation described in the coursework description.
-# Given the dataframe df with the data to be clustered,
-# return a pandas dataframe with an entry for each clustering algorithm execution.
-# Each entry should contain the: 
-# 'Algorithm' name: either 'Kmeans' or 'Agglomerative', 
-# 'data' type: either 'Original' or 'Standardized',
-# 'k': the number of clusters produced,
-# 'Silhouette Score': for evaluating the resulting set of clusters.
-def cluster_evaluation(df):
+# Given a dataframe where each tweet is one string with words separated by single whitespaces,
+# tokenize every tweet by converting it into a list of words (strings).
+def tokenize(df):
 	pass
 
-# Given the performance evaluation dataframe produced by the cluster_evaluation function,
-# return the best computed Silhouette score.
-def best_clustering_score(rdf):
+# Given dataframe tdf with the tweets tokenized, return the number of words in all tweets including repetitions.
+def count_words_with_repetitions(tdf):
 	pass
 
-# Run the Kmeans algorithm with k=3 by using the standardized data set.
-# Generate a scatter plot for each pair of attributes.
-# Data points in different clusters should appear with different colors.
-def scatter_plots(df):
+# Given dataframe tdf with the tweets tokenized, return the number of distinct words in all tweets.
+def count_words_without_repetitions(tdf):
 	pass
+
+# Given dataframe tdf with the tweets tokenized, return a list with the k distinct words that are most frequent in the tweets.
+def frequent_words(tdf,k):
+	pass
+
+# Given dataframe tdf with the tweets tokenized, remove stop words and words with <=2 characters from each tweet.
+# The function should download the list of stop words via:
+# https://raw.githubusercontent.com/fozziethebeat/S-Space/master/data/english-stop-words-large.txt
+def remove_stop_words(tdf):
+	pass
+
+# Given dataframe tdf with the tweets tokenized, reduce each word in every tweet to its stem.
+def stemming(tdf):
+	pass
+
+# Given a pandas dataframe df with the original coronavirus_tweets.csv data set,
+# build a Multinomial Naive Bayes classifier. 
+# Return predicted sentiments (e.g. 'Neutral', 'Positive') for the training set
+# as a 1d array (numpy.ndarray). 
+def mnb_predict(df):
+	pass
+
+# Given a 1d array (numpy.ndarray) y_pred with predicted labels (e.g. 'Neutral', 'Positive') 
+# by a classifier and another 1d array y_true with the true labels, 
+# return the classification accuracy rounded in the 3rd decimal digit.
+def mnb_accuracy(y_pred,y_true):
+	pass
+
+
+
+
 
